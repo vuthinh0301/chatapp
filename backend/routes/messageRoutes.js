792e6a8f2a79fd5b0dd3ee0b8c36c6fe2bae824c
vuthinh0301 <vuthinh0301@gmail.com>
2022-08-26 02:57:15 +0700

    init project

---
+++ backend/routes/messageRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const { protect } = require("../middleware/authMiddleware");
+const {sendMessage, allMessages} = require("../controllers/messageControlers");
+
+const router = express.Router();
+
+router.route("/:chatId").get(protect, allMessages);
+router.route("/").post(protect, sendMessage);
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const chats = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require('./routes/userRoutes')
+const chatRoutes = require('./routes/chatRoutes')
+const messageRoutes = require('./routes/messageRoutes')
+const {notFound, errorHandler} = require("./middleware/errorMiddleware");
+
+
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+})
+
+app.use('/api/user', userRoutes)
+app.use('/api/chat', chatRoutes)
+app.use('/api/message', messageRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT
+app.listen(5000, console.log(`Server start on port ${PORT}`));
+
+
